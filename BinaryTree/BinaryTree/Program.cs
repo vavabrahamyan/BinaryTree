3d4f7f6a0faf415566df0a869bd24a3e6f9010c5
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-07-13 01:01:01 +0400

    ~

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BinaryTree
+{
+    class Tree 
+    {
+        public Tree(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; set; }
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+        public void Add(int value)
+        {
+            if(value < this.Value)
+            {
+                if(Left == null)
+                {
+                    Left = new Tree(value);
+                }
+                else
+                {
+                    Left.Add(value);
+                }
+            }
+            if(value >= this.Value)
+            {
+                if(Right == null)
+                {
+                    Right = new Tree(value);
+                }
+                else
+                {
+                    Right.Add(value);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 5, 9, 8, 6 };
+            Tree tree = new Tree(7);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tree.Add(arr[i]);
+            }
+        }
+    }
+}
